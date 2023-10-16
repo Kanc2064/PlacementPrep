1daf7ee67f9bf642d26fcce9b3a78cb5aecf54b8
Kanc2064 <139121950+Kanc2064@users.noreply.github.com>
2023-10-16 16:44:25 +0530

    Add Login files

---
+++ Login.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+import "./login.css";
+
+export default function login() {
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or Phone Number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>
+            New to PlacementPrep? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by Google reCaptcha to ensure you're not a
+            bot.
+            <b>Learn more</b>.
+          </small>
+        </form>
+      </div>
+    </div>
+  );
+}
