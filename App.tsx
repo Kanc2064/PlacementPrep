51508a0d21373027cd0846f30afec2930754775a
Kanc2064 <139121950+Kanc2064@users.noreply.github.com>
2023-10-16 16:42:49 +0530

    add src file

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
