3e423e2a1a808b2aa263a731b4d7bf25180fbb15
Kanc2064 <139121950+Kanc2064@users.noreply.github.com>
2023-10-16 16:43:52 +0530

    Add home files

---
+++ Home.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-template-curly-in-string */
+import "./home.css";
+// import Featured from "../../components/featured/Featured";
+// import List from "../../components/list/List";
+import Navbar from "../../components/navbar/Navbar";
+// import axios from "axios";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
